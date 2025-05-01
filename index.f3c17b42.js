1ed225642b268a68b9c13e2e556905683d847fc6
Nastya Humeniuk <perevalova43@icloud.com>
2025-05-01 17:12:35 +0300

    make build

---
+++ index.f3c17b42.js
@@ -0,0 +1,2 @@
+!function(){for(var e=function(e){r[e].addEventListener("click",function(){for(var t=0;t<l.length;t++){var r=l[t].querySelectorAll("td");r[e]&&r[e].classList.toggle("highlight")}})},t=document.querySelector(".myTable"),l=t.querySelectorAll("tr"),r=t.querySelector("thead").querySelectorAll("th"),c=0;c<r.length;c++)e(c)}();
+//# sourceMappingURL=index.f3c17b42.js.map
